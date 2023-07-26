5ee2f9aae147e89ac74f2ca7d72dd4290ec64413
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-07-26 17:54:56 +0200

    Add config items

--- src/summarizationmodel/config.py
+++ src/summarizationmodel/config.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 ROOT_DIR = Path().resolve()
-TOKENIZER_DIR = ROOT_DIR / "src" / "summarizationmodel" / "trained_tokenizer"
 DATA_DIR = ROOT_DIR / "data"
+TOKENIZER_DIR = ROOT_DIR / "src" / "summarizationmodel" / "trained_tokenizer"
 
-UNK_TOKEN = "[UNK]"
 PAD_TOKEN = "[PAD]"
+UNK_TOKEN = "[UNK]"
 START_TOKEN = "[START]"
 END_TOKEN = "[STOP]"
 
@@ -15,6 +15,10 @@ VOCAB_SIZE = 50000
 MAX_ENCODER_STEPS = 400
 MAX_DECODER_STEPS = 100
 MODEL_MAX_LENGTH = 2048
+HIDDEN_DIM = 256
+EMBEDDING_DIM = 128
+BEAM_SIZE = 4
+MIN_DEC_STEPS = 35
 
 PADDABLE_FEATURES = [
     "encoder_input_ids",

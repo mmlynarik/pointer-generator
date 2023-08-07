a586fc329a501f58e23d919ff13e6c8ed5928007
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-08-08 01:07:00 +0200

    Add decoder to model

--- src/summarizationmodel/model.py
+++ src/summarizationmodel/model.py
@@ -1,7 +1,7 @@
-from typing import Any
+from typing import Any, Optional, NamedTuple
 from dataclasses import dataclass
 
-import torch
+import torch as pt
 from torch import nn
 from lightning.pytorch import LightningModule
 
@@ -18,54 +18,61 @@ from summarizationmodel.config import (
 )
 
 
-@dataclass
-class LSTMStateTuple:
-    """
-    LSTM encoder final state with both attributes of shape [batch_size, actual_hidden_dim].
-    Actual_hidden_dim = 2 * hidden_dim (for unreduced state) or hidden_dim (for reduced state).
-    """
+class LSTMState(NamedTuple):
+    """Single-layer LSTM state consisting of two attributes of shape [batch_size, current_hidden_dim]."""
 
-    hidden_state: torch.Tensor
-    cell_state: torch.Tensor
+    hidden_state: pt.Tensor
+    cell_state: pt.Tensor
+
+    @property
+    def concatenated(self) -> pt.Tensor:
+        return pt.cat([self.hidden_state, self.cell_state], dim=1)
 
 
-class Embedding(nn.Module):
+class SharedEmbedding(nn.Module):
     def __init__(self, embedding_dim: int, vocab_size: int, pad_token_id: int):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim, pad_token_id)
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+    def forward(self, inputs: pt.Tensor) -> pt.Tensor:
         return self.embedding(inputs)
+
+    @property
+    def embedding_dim(self) -> int:
+        return self.embedding.embedding_dim
 
 
 class PointerGeneratorStateReducer(nn.Module):
-    """Dense layer reducing encoder final state from 2 * hidden_dim down to hidden_dim."""
+    """Dense layer reducing encoder final state dimension from 2 * hidden_dim down to hidden_dim."""
 
     def __init__(self, hidden_dim: int):
         super().__init__()
-        self.linear_cell = nn.Linear(2 * hidden_dim, hidden_dim)
-        self.linear_hidden = nn.Linear(2 * hidden_dim, hidden_dim)
+        self.cell = nn.Linear(2 * hidden_dim, hidden_dim)
+        self.hidden = nn.Linear(2 * hidden_dim, hidden_dim)
+        self.relu = nn.ReLU()
 
-    def forward(self, lstm_state: LSTMStateTuple) -> LSTMStateTuple:
-        reduced_hidden_state = self.linear_hidden(lstm_state.hidden_state)
-        reduced_cell_state = self.linear_cell(lstm_state.cell_state)
-        return LSTMStateTuple(reduced_hidden_state, reduced_cell_state)
+    def forward(self, lstm_state: LSTMState) -> LSTMState:
+        reduced_hidden_state = self.relu(self.hidden(lstm_state.hidden_state))
+        reduced_cell_state = self.relu(self.cell(lstm_state.cell_state))
+        return LSTMState(reduced_hidden_state, reduced_cell_state)
 
 
 class PointerGeneratorEncoder(nn.Module):
-    """Single-layer bi-directional LSTM encoder.
+    """Single-layer bi-directional LSTM encoder with PackedSequence inputs to LSTM instead of dynamic padding
 
-    Inputs: tensor of shape [batch_size, max_enc_steps, embedding_dim].
+    Inputs:
+        input_ids: Article token ids of shape [batch_size, max_enc_steps]
+        padding_mask: Boolean mask of shape [batch_size, max_enc_steps]
     Outputs:
-        encoder_outputs of shape [batch_size, max_enc_steps, 2 * hidden_dim]
-        unreduced lstm_state with attributes of shape [batch_size, 2 * hidden_dim]
+        outputs: Tensor of shape [batch_size, max_enc_steps, 2 * hidden_dim]
+        last_state: Last LSTMState with attributes of shape [batch_size, 2 * hidden_dim]
     """
 
-    def __init__(self, hidden_dim: int, embedding_dim: int, vocab_size: int, pad_token_id: int):
+    def __init__(self, hidden_dim: int, embedding: SharedEmbedding):
         super().__init__()
-        self.embedding = Embedding(embedding_dim, vocab_size, pad_token_id)
+        self.embedding = embedding
         self.lstm = nn.LSTM(
-            input_size=embedding_dim,
+            input_size=embedding.embedding_dim,
             hidden_size=hidden_dim,
             num_layers=1,
             bidirectional=True,
@@ -73,57 +80,202 @@ class PointerGeneratorEncoder(nn.Module):
         )
         self.state_reducer = PointerGeneratorStateReducer(hidden_dim)
 
-    def forward(self, inputs: torch.Tensor) -> tuple[torch.Tensor, LSTMStateTuple]:
-        input_ids, mask = inputs["encoder_input_ids"], inputs["encoder_padding_mask"]
+    def forward(self, input_ids: pt.Tensor, padding_mask: pt.Tensor) -> tuple[pt.Tensor, LSTMState]:
         embeddings = self.embedding(input_ids)
-        lengths = self.get_input_lengths(mask)
+        lengths = self.get_input_lengths(padding_mask)
 
         packed_embeddings = nn.utils.rnn.pack_padded_sequence(embeddings, lengths, True, False)
-        packed_encoder_outputs, (hidden_state, cell_state) = self.lstm(packed_embeddings)
-        encoder_outputs, _ = nn.utils.rnn.pad_packed_sequence(packed_encoder_outputs)
+        packed_outputs, (hidden_state, cell_state) = self.lstm(packed_embeddings)
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(packed_outputs, batch_first=True)
 
-        lstm_state_tuple = self.get_lstm_state_tuple_from_final_state(hidden_state, cell_state)
-        decoder_init_state = self.state_reducer(lstm_state_tuple)
-        return encoder_outputs, decoder_init_state
+        last_state = self.get_lstm_state_from_components(hidden_state, cell_state)
+        last_state = self.state_reducer(last_state)
+        return outputs, last_state
 
-    def get_input_lengths(self, encoder_padding_mask: torch.Tensor) -> torch.Tensor:
-        return encoder_padding_mask.sum(dim=1)
+    def get_input_lengths(self, padding_mask: pt.Tensor) -> pt.Tensor:
+        return padding_mask.sum(dim=1)
 
-    def get_lstm_state_tuple_from_final_state(
-        self, hidden_state: torch.Tensor, cell_state: torch.Tensor
-    ) -> LSTMStateTuple:
-        hidden_state = torch.cat([direction for direction in hidden_state], axis=1).squeeze()
-        cell_state = torch.cat([direction for direction in cell_state], axis=1).squeeze()
-        return LSTMStateTuple(hidden_state, cell_state)
+    def get_lstm_state_from_components(self, hidden_state: pt.Tensor, cell_state: pt.Tensor) -> LSTMState:
+        hidden_state = pt.cat([direction for direction in hidden_state], axis=1).squeeze()
+        cell_state = pt.cat([direction for direction in cell_state], axis=1).squeeze()
+        return LSTMState(hidden_state, cell_state)
+
+
+class BahdanauAttnWithCoverage(nn.Module):
+    """Bahdanau attention layer used to compute context vector, attention distribution and coverage vector."""
+
+    def __init__(self, hidden_dim: int):
+        super().__init__()
+        self.Wh = nn.Linear(2 * hidden_dim, hidden_dim, bias=False)
+        self.Ws = nn.Linear(2 * hidden_dim, hidden_dim, bias=True)
+        self.v = nn.Linear(hidden_dim, 1, bias=False)
+        self.wc = nn.Linear(1, hidden_dim, bias=False)
+
+    def forward(
+        self,
+        encoder_outputs: pt.Tensor,
+        encoder_padding_mask: pt.Tensor,
+        decoder_state: LSTMState,
+        coverage: Optional[pt.Tensor] = None,
+    ):
+        coverage = pt.zeros_like(encoder_padding_mask) if coverage is None else coverage
+
+        decoder_state = decoder_state.concatenated.unsqueeze(dim=1)
+        encoder_padding_mask = encoder_padding_mask.unsqueeze(dim=2)
+        coverage = coverage.unsqueeze(dim=2)
+
+        encoder_features = self.Wh(encoder_outputs)
+        decoder_features = self.Ws(decoder_state)
+        coverage_features = self.wc(coverage)
+        attn_scores = self.v(pt.tanh(encoder_features + decoder_features + coverage_features))  # B-L-1
+
+        masked_scores = attn_scores.masked_fill(encoder_padding_mask, -float("inf"))
+        attn_dist = nn.functional.softmax(masked_scores, dim=1)  # B-L-1
+
+        context = attn_dist * encoder_outputs
+        context = context.sum(dim=1)  # B-2H
+        coverage += attn_dist
+
+        return context, attn_dist.squeeze(), coverage
+
+
+class BahdanauAttn(nn.Module):
+    """Bahdanau attention layer used to compute context vector, attention distribution and coverage vector."""
+
+    def __init__(self, hidden_dim: int):
+        super().__init__()
+        self.Wh = nn.Linear(2 * hidden_dim, hidden_dim, bias=False)
+        self.Ws = nn.Linear(2 * hidden_dim, hidden_dim, bias=True)
+        self.v = nn.Linear(hidden_dim, 1, bias=False)
+
+    def forward(
+        self,
+        encoder_outputs: pt.Tensor,
+        encoder_padding_mask: pt.Tensor,
+        decoder_state: LSTMState,
+        coverage: Optional[pt.Tensor] = None,
+    ):
+        decoder_state = decoder_state.concatenated.unsqueeze(dim=1)
+        encoder_padding_mask = encoder_padding_mask.unsqueeze(dim=2)
+
+        encoder_features = self.Wh(encoder_outputs)
+        decoder_features = self.Ws(decoder_state)
+        attn_scores = self.v(pt.tanh(encoder_features + decoder_features))  # B-L-1
+
+        masked_scores = attn_scores.masked_fill(encoder_padding_mask, -float("inf"))
+        attn_dist = nn.functional.softmax(masked_scores, dim=1)  # B-L-1
+
+        context = attn_dist * encoder_outputs
+        context = context.sum(dim=1)  # B-2H
+
+        return context, attn_dist.squeeze(), None
+
+
+class VocabularyDistribution(nn.Module):
+    """Reduce concatenated LSTM output and then apply final transformation to vocabulary distribution."""
+
+    def __init__(self, hidden_dim: int, vocab_size: int):
+        super().__init__()
+        self.reducer = nn.Linear(3 * hidden_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, vocab_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, inputs: pt.Tensor) -> pt.Tensor:
+        x = self.reducer(inputs)
+        x = self.output(x)
+        x = self.softmax(x)
+        return x
+
+
+class GenerationProbability(nn.Module):
+    """Calculate generation probability from state, input and and context vectors."""
+
+    def __init__(self, hidden_dim: int):
+        super().__init__()
+        self.wh = nn.Linear(2 * hidden_dim, 1, bias=False)
+        self.ws = nn.Linear(2 * hidden_dim, 1, bias=False)
+        self.wx = nn.Linear(hidden_dim, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: pt.Tensor, state: pt.Tensor, context: pt.Tensor) -> pt.Tensor:
+        return self.sigmoid(self.wh(context) + self.ws(state) + self.wx(x))
 
 
 class PointerGeneratorDecoder(nn.Module):
-    """Single-layer unidirectional LSTM decoder with attention.
+    """Single-layer unidirectional LSTM decoder with attention, pointer and coverage mechanism.
 
-    Inputs: tensor of shape [batch_size, max_enc_steps, embedding_dim].
+    Inputs:
+        input_ids: Summary token ids of shape [batch_size, max_dec_steps]
+        initial_state: Initial LSTMState with attributes of shape [batch_size, hidden_dim]
+        encoder_outputs: Encoder outputs tensor of shape [batch_size, max_enc_steps, 2*hidden_dim]
+        encoder_mask: Boolean padding mask of shape [batch_size, max_enc_steps]
+        prev_coverage: Previous coverage vector, used during decoding of shape [batch_size, attention_length]
     Outputs:
-        encoder_outputs of shape [batch_size, max_enc_steps, 2 * hidden_dim]
-        unreduced lstm_state with attributes of shape [batch_size, 2 * hidden_dim]
+        vocab_dists: Vocabulary distributions of shape [batch_size, max_dec_steps, vocab_size]
+        attn_dists: Attention distributions of shape [batch_size, max_dec_steps, max_enc_steps]
+        pgens: Generation probabilities of shape [batch_size, max_dec_steps]
+        coverage: Coverage vector of shape [batch_size, 2 * hidden_dim]
+        state: Last decoder LSTMState
     """
 
-    def __init__(self, hidden_dim: int, embedding_dim: int, vocab_size: int, pad_token_id: int):
+    def __init__(
+        self,
+        hidden_dim: int,
+        vocab_size: int,
+        embedding: SharedEmbedding,
+        initial_state_attention: bool = False,
+        use_coverage: bool = False,
+    ):
         super().__init__()
-        self.embedding = Embedding(embedding_dim, vocab_size, pad_token_id)
-        self.decoder = nn.LSTM(
-            input_size=embedding_dim,
-            hidden_size=hidden_dim,
-            num_layers=1,
-            batch_first=True,
-        )
+        self.embedding = embedding
+        self.initial_state_attention = initial_state_attention
 
-    def forward(self, inputs: torch.Tensor) -> tuple[torch.Tensor, LSTMStateTuple]:
-        encoder_embeddings = self.embedding(inputs["encoder_input_ids"])
-        encoder_outputs, (hidden_state, cell_state) = self.encoder(encoder_embeddings)
-        hidden_state = torch.cat([direction for direction in hidden_state], axis=1).squeeze()
-        cell_state = torch.cat([direction for direction in cell_state], axis=1).squeeze()
-        lstm_state = LSTMStateTuple(hidden_state, cell_state)
-        decoder_init_state = self.state_reducer(lstm_state)
-        return encoder_outputs, decoder_init_state
+        self.reducer = nn.Linear(2 * hidden_dim + embedding.embedding_dim, hidden_dim)
+        self.lstm_cell = nn.LSTMCell(hidden_dim, hidden_dim)
+        self.attention = BahdanauAttnWithCoverage(hidden_dim) if use_coverage else BahdanauAttn(hidden_dim)
+
+        self.pgen = GenerationProbability(hidden_dim)
+        self.vocab_dist = VocabularyDistribution(hidden_dim, vocab_size)
+
+    def forward(
+        self,
+        input_ids: pt.Tensor,
+        initial_state: LSTMState,
+        encoder_outputs: pt.Tensor,
+        encoder_mask: pt.Tensor,
+        prev_coverage: Optional[pt.Tensor] = None,
+    ) -> tuple[pt.Tensor, LSTMState]:
+        vocab_dists, attn_dists, pgens = [], [], []
+
+        state: LSTMState = initial_state
+        coverage = prev_coverage
+        context = pt.zeros(encoder_outputs.size()[0], encoder_outputs.size()[2])
+
+        if self.initial_state_attention:  # true in decode mode
+            context, _, coverage = self.attention(encoder_outputs, encoder_mask, state, coverage)
+
+        embeddings = self.embedding(input_ids)
+        for step, step_embeddings in enumerate(embeddings.transpose(0, 1)):
+            concatenated_input = pt.cat([step_embeddings, context], dim=1)
+            x = self.reducer(concatenated_input)
+            state = self.lstm_cell(x, state)
+            state = LSTMState(state[0], state[1])
+
+            if step == 0 and self.initial_state_attention:
+                context, attn_dist, _ = self.attention(encoder_outputs, encoder_mask, state, coverage)
+            else:
+                context, attn_dist, coverage = self.attention(encoder_outputs, encoder_mask, state, coverage)
+
+            concatenated_lstm_output = pt.cat([state.hidden_state, context], dim=1)
+            attn_dists.append(attn_dist)
+            pgens.append(self.pgen(x, state.concatenated, context))
+            vocab_dists.append(self.vocab_dist(concatenated_lstm_output))
+
+        vocab_dists = pt.stack(vocab_dists, dim=1)
+        attn_dists = pt.stack(attn_dists, dim=1)
+        pgens = pt.stack(pgens, dim=1)
+
+        return vocab_dists, attn_dists, pgens, coverage, state
 
 
 class PointerGeneratorSummarizatonModel(nn.Module):
@@ -146,12 +298,19 @@ class PointerGeneratorSummarizatonModel(nn.Module):
         # fmt: on
     ):
         super().__init__()
-        self.encoder = PointerGeneratorEncoder(hidden_dim, embedding_dim, vocab_size, pad_token_id)
-        self.decoder = PointerGeneratorDecoder(hidden_dim, embedding_dim, vocab_size, pad_token_id)
+        embedding = SharedEmbedding(embedding_dim, vocab_size, pad_token_id)
+        self.encoder = PointerGeneratorEncoder(hidden_dim, embedding)
+        self.decoder = PointerGeneratorDecoder(hidden_dim, vocab_size, embedding, False, False)
 
     def forward(self, inputs: dict[str, Any]):
-        encoder_outputs, decoder_init_state = self.encoder(inputs)
-        return encoder_outputs, decoder_init_state
+        encoder_input_ids, encoder_padding_mask = inputs["encoder_input_ids"], inputs["encoder_padding_mask"]
+        decoder_input_ids = inputs["decoder_input_ids"]
+
+        encoder_outputs, encoder_last_state = self.encoder(encoder_input_ids, encoder_padding_mask)
+        vocab_dists, attn_dists, pgens, coverage, state = self.decoder(
+            decoder_input_ids, encoder_last_state, encoder_outputs, encoder_padding_mask, None
+        )
+        return vocab_dists
 
 
 class AbstractiveSummarizationModel(LightningModule):
@@ -193,8 +352,8 @@ def main():
 
     for step, batch in enumerate(dm.train_dataloader()):
         if step == 0:
-            encoder_output, decoder_init_state = model(batch)
-            print(encoder_output)
+            encoder_output = model(batch)
+            print(encoder_output.shape)
             break
 
 
---
+++ src/summarizationmodel/utils.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def gather_nd(params, indices):
+    """Tensorflow gather_nd function ported to Pytorch."""
+
+    out_shape = indices.shape[:-1]
+    indices = indices.unsqueeze(0).transpose(0, -1) # roll last axis to fring
+    ndim = indices.shape[0]
+    indices = indices.long()
+    idx = torch.zeros_like(indices[0], device=indices.device).long()
+    m = 1
+
+    for i in range(ndim)[::-1]:
+        idx += indices[i] * m
+        m *= params.size(i)
+    out = torch.take(params, idx)
+    return out.view(out_shape)

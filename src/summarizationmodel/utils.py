de2c72cd923c1cc9262e93e699b09465000b05a4
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-08-11 20:36:47 +0200

    Add utils

--- src/summarizationmodel/utils.py
+++ src/summarizationmodel/utils.py
@@ -1,4 +1,7 @@
-import torch
+from functools import wraps
+import time
+
+import torch as pt
 
 
 def gather_nd(params, indices):
@@ -8,11 +11,27 @@ def gather_nd(params, indices):
     indices = indices.unsqueeze(0).transpose(0, -1) # roll last axis to fring
     ndim = indices.shape[0]
     indices = indices.long()
-    idx = torch.zeros_like(indices[0], device=indices.device).long()
+    idx = pt.zeros_like(indices[0], device=indices.device).long()
     m = 1
 
     for i in range(ndim)[::-1]:
         idx += indices[i] * m
         m *= params.size(i)
-    out = torch.take(params, idx)
+    out = pt.take(params, idx)
     return out.view(out_shape)
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+
+def is_finite(tensor: pt.Tensor) -> bool:
+    return pt.all(pt.isfinite(tensor)).item()

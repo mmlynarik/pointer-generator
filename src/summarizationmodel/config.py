7aa36f514d78b75b578d168352ae28f913010f28
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-08-12 21:03:05 +0200

    Simplify config

--- src/summarizationmodel/config.py
+++ src/summarizationmodel/config.py
@@ -1,32 +1,22 @@
 from pathlib import Path
 
+# Folder config
 ROOT_DIR = Path().resolve()
 DATA_DIR = ROOT_DIR / "data"
 TOKENIZER_DIR = ROOT_DIR / "src" / "summarizationmodel" / "trained_tokenizer"
 
+# Tokenizer config
 PAD_TOKEN = "[PAD]"
 UNK_TOKEN = "[UNK]"
 START_TOKEN = "[START]"
 END_TOKEN = "[STOP]"
-
-SPECIAL_TOKENS = [PAD_TOKEN, UNK_TOKEN, START_TOKEN, END_TOKEN]
-
 VOCAB_SIZE = 50000
 MAX_ENCODER_STEPS = 400
 MAX_DECODER_STEPS = 100
 MODEL_MAX_LENGTH = 2048
+
+# Model config
 HIDDEN_DIM = 256
 EMBEDDING_DIM = 128
 BEAM_SIZE = 4
 MIN_DEC_STEPS = 35
-
-PADDABLE_FEATURES = [
-    "encoder_input_ids",
-    "encoder_padding_mask",
-    "decoder_input_ids",
-    "decoder_padding_mask",
-    "decoder_target_ids",
-    "encoder_inputs_extvoc",
-]
-
-NON_PADDABLE_FEATURES = ["oovs"]

0e7166be411fc247630af3dc1246128ba2e163fb
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-08-12 21:03:39 +0200

    Simplify utils

--- src/summarizationmodel/utils.py
+++ src/summarizationmodel/utils.py
@@ -4,23 +4,6 @@ import time
 import torch as pt
 
 
-def gather_nd(params, indices):
-    """Tensorflow gather_nd function ported to Pytorch."""
-
-    out_shape = indices.shape[:-1]
-    indices = indices.unsqueeze(0).transpose(0, -1) # roll last axis to fring
-    ndim = indices.shape[0]
-    indices = indices.long()
-    idx = pt.zeros_like(indices[0], device=indices.device).long()
-    m = 1
-
-    for i in range(ndim)[::-1]:
-        idx += indices[i] * m
-        m *= params.size(i)
-    out = pt.take(params, idx)
-    return out.view(out_shape)
-
-
 def timeit(func):
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):

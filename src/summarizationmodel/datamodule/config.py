1e5064d3fda638ac5f0e7f4c49025a6d029d5ed1
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-08-13 23:59:49 +0200

    Simplify config

--- src/summarizationmodel/datamodule/config.py
+++ src/summarizationmodel/datamodule/config.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 
-# Folder config
+
 ROOT_DIR = Path().resolve()
 DATA_DIR = ROOT_DIR / "data"
 TOKENIZER_DIR = ROOT_DIR / "src" / "summarizationmodel" / "trained_tokenizer"
 
-# Tokenizer config
 PAD_TOKEN = "[PAD]"
 UNK_TOKEN = "[UNK]"
 START_TOKEN = "[START]"
@@ -15,9 +14,3 @@ MAX_ENCODER_STEPS = 400
 MAX_DECODER_STEPS = 100
 MODEL_MAX_LENGTH = 2048
 PAD_TOKEN_ID = 0
-
-# Model config
-HIDDEN_DIM = 256
-EMBEDDING_DIM = 128
-BEAM_SIZE = 4
-MIN_DEC_STEPS = 35
